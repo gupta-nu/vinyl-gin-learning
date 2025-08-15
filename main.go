ecc52df4f530f8259cb15d8816af2f1532de6692
gupta-nu <gupta.ananya@protonmail.com>
2025-08-15 15:43:23 +0530

    get by id configuration

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 type album struct {
 	ID     string  `json:"id"`
-	Title  string  `json: "title"`
-	Artist string  `json: "artist"`
-	Price  float64 `json: "price"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 //albums slice to seed record ablum data
@@ -45,9 +45,29 @@ func postAlbums(c *gin.Context) {
 	//statusCreated is for pos requests that add new resources
 }
 
+//logic to return specific item ,to retrieve requested album ,
+//to map the path to logic
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a) //sends back a 200 ok status to client, and returns a
+			return
+		}
+	}
+	//if no match, 404 respone it send and
+	//gin.H is map[string]interface{} , - its a go map, key-string, value- anything,
+	//so we send gin.H of the message of album not found
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default() // initi gin rounter
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
+
 	router.Run("localhost:8080")
 }
